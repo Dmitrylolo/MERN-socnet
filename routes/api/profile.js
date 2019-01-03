a14eca61832ca8782fcec802b1014892f6384804
Dmitry Chernenkov <dmitrychee@gmail.com>
2019-01-03 20:48:58 +0300

    education and expereience delete routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -246,6 +246,54 @@ router.post(
   }
 );
 
+//@router DELETE api/profile/experience/:exp_id
+//@desc   Delete experience from profile
+//@access Privat
+router.delete(
+  '/experience/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.experience
+          .map(exp => exp.id)
+          .indexOf(req.params.exp_id);
+
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(error));
+  }
+);
+
+//@router DELETE api/profile/education/:edu_id
+//@desc   Delete experience from profile
+//@access Privat
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.education
+          .map(edu => edu.id)
+          .indexOf(req.params.edu_id);
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 //@router GET api/profile/test
 //@desc Tetst profile route
 //@access Public
