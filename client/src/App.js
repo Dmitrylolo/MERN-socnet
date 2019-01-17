14252437a88d1fdb491262086caa224c1800868c
Dmitry Chernenkov <dmitrychee@gmail.com>
2019-01-17 21:17:05 +0200

    add post, get posts

--- client/src/App.js
+++ client/src/App.js
@@ -21,10 +21,11 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import AddEducation from './components/add-credentials/AddEducation';
 import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
 
 import './App.css';
-import AddEducation from './components/add-credentials/AddEducation';
 
 // Check token
 
@@ -89,6 +90,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import profileReducer from './profileReducer';
 import errorReducer from './errorReducer';
-import postReducer from './postsReducer';
+import postReducer from './postReducer';
 
 export default combineReducers({
   auth: authReducer,
